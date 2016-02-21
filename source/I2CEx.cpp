e490e9f2df25347c4d731547ad51cd9efd2c2ec6
Marcus Chang <marcus.chang@arm.com>
2016-02-21 02:59:55 +0000

    Added shared modules, new naming convention, and new i2c api.

---
+++ source/I2CEx.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "wrd-utilities/I2CEx.h"
+
+
+I2CEx::I2CEx(PinName sda, PinName scl)
+    :   I2C(sda, scl),
+        transactionInProgress(false),
+        processQueueHandle(NULL)
+{
+
+}
+
+I2CEx::~I2CEx()
+{
+    if (processQueueHandle)
+    {
+        minar::Scheduler::cancelCallback(processQueueHandle);
+    }
+}
+
+void I2CEx::read(int address,
+                 int reg,
+                 char* buffer,
+                 int length,
+                 FunctionPointer0<void> callback)
+{
+    addQueue(I2CEx::READ, address, reg, buffer, length, callback);
+}
+
+void I2CEx::write(int address,
+                  int reg,
+                  char* buffer,
+                  int length,
+                  FunctionPointer0<void> callback)
+{
+    addQueue(I2CEx::WRITE, address, reg, buffer, length, callback);
+}
+
+void I2CEx::addQueue(action_t action,
+                     int address,
+                     int reg,
+                     char* buffer,
+                     int length,
+                     FunctionPointer0<void> callback)
+{
+    /* add command to queue */
+    I2CEx::transaction_t transaction;
+
+    transaction.action   = action;
+    transaction.address  = address;
+    transaction.reg      = reg;
+    transaction.buffer   = buffer;
+    transaction.length   = length;
+    transaction.callback = callback;
+
+    sendQueue.push(transaction);
+
+    /* schedule queue to be processed */
+    if (transactionInProgress == false)
+    {
+        transactionInProgress = true;
+        processQueueHandle = minar::Scheduler::postCallback(this, &I2CEx::processQueue)
+                                .tolerance(1)
+                                .getHandle();
+    }
+}
+
+void I2CEx::processQueue(void)
+{
+    /* clear task handle */
+    processQueueHandle = NULL;
+
+    /* only process if queue is not empty */
+    if (sendQueue.size() > 0)
+    {
+        I2C::event_callback_t callback(this, &I2CEx::i2cDone);
+
+        /* get action */
+        I2CEx::transaction_t transaction = sendQueue.front();
+
+        /* */
+        if (transaction.action == I2CEx::READ)
+        {
+            writeBuffer[0] = transaction.reg;
+
+            I2C::transfer(transaction.address,
+                          writeBuffer, 1,
+                          transaction.buffer, transaction.length,
+                          callback);
+        }
+        else
+        {
+            writeBuffer[0] = transaction.reg;
+            memcpy(&writeBuffer[1], transaction.buffer, transaction.length);
+
+            I2C::transfer(transaction.address,
+                          writeBuffer, transaction.length + 1,
+                          transaction.buffer, 0,
+                          callback);
+        }
+    }
+    else
+    {
+        transactionInProgress = false;
+    }
+}
+
+void I2CEx::i2cDone(Buffer txBuffer, Buffer rxBuffer, int code)
+{
+    (void) txBuffer;
+    (void) rxBuffer;
+    (void) code;
+
+    /* get action */
+    I2CEx::transaction_t transaction = sendQueue.front();
+
+    /* remove action from queue */
+    sendQueue.pop();
+
+    /* signal callback */
+    minar::Scheduler::postCallback(transaction.callback);
+
+    /* process next action */
+    processQueueHandle = minar::Scheduler::postCallback(this, &I2CEx::processQueue)
+                            .tolerance(1)
+                            .getHandle();
+}
---
+++ wrd-utilities/SharedModules.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __WRD_SHARED_MODULES_H__
+#define __WRD_SHARED_MODULES_H__
+
+#include "mbed-drivers/mbed.h"
+#include "wrd-utilities/I2CEx.h"
+
+#if YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_I2C_SIZE > 0
+extern I2CEx YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_I2C_I2C0_NAME;
+#endif
+
+#if YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_SPI_SIZE > 0
+extern SPI YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_SPI_SPI0_NAME;
+#endif
+
+#if YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_SPI_SIZE > 1
+extern SPI YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_SPI_SPI1_NAME;
+#endif
+
+#if YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_SPI_SIZE > 2
+extern SPI YOTTA_CFG_HARDWARE_WEARABLE_REFERENCE_DESIGN_INTERFACE_SPI_SPI2_NAME;
+#endif
+
+#endif // __WRD_SHARED_MODULES_H__
